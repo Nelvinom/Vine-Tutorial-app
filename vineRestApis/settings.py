335b01f58fb913a7b342b878d12d62cb94ab3af6
Nelvinom <omondinelvin884@gmail.com>
2020-06-10 10:45:45 +0300

    new app tutorials

--- vineRestApis/settings.py
+++ vineRestApis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +75,11 @@ WSGI_APPLICATION = 'vineRestApis.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE':'django.db.backends.  postgresql',
+        'NAME': 'vine',
+        'USER': 'moringa',
+        'PASSWORD':'@young',
     }
 }
 
