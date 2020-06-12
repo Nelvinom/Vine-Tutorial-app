c6e74d8c3961064cfb016d0011e53124a765196f
BA <engineervinceblair@gmail.com>
2020-06-12 05:35:47 +0300

    added atutorial service.create method in tutorial html

--- Angular8ClientCrud/src/app/components/add-tutorial/add-tutorial.component.ts
+++ Angular8ClientCrud/src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-tutorial.component.css']
 })
 export class AddTutorialComponent implements OnInit {
+  tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private tutorialService: TutorialService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  saveTutorial() {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newTutorial() {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false
+    };
+  }
 }
